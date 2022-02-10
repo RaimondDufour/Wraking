dbbed78f548fae21b3677ac4db9e89742ac32126
RaimondDufour <dufourraimond@gmail.com>
2022-02-10 22:54:30 +0100

    Add files via upload

--- Query ophalen en opslaan.R
+++ Query ophalen en opslaan.R
@@ -5,7 +5,8 @@ setwd("D:/Uitwisselmap/Projecten/Ecli/Rstudio")
 source ("D://Uitwisselmap/Projecten/Rstudio/connecties.R")
 library(RMySQL)
 library(tm)
-
+library(stringr)
+library(tidyr)
 # uitkomst en redenen: 
 WrakingsRedenenQuery <- paste(
   "SELECT onderzoek.ecli, tekst1 as wrakingszaak, tekst2 as uitkomst, categorie1 as reden, inhoudsindicatie, uitspraak
@@ -45,6 +46,65 @@ saveRDS(Rechtsgebieden, file = "D:\\Uitwisselmap\\Projecten\\Ecli\\Rstudio\\Wrak
 Dataset <- readRDS(file = "D:\\Uitwisselmap\\Projecten\\Ecli\\Rstudio\\Wraking_Rechtsgebieden.rds") 
 
 
+
+# met veel andere variabelen:
+WrakingVeelVariabelenQuery <- paste(
+  "SELECT onderzoek.ecli, tekst1 as wrakingszaak, tekst2 as uitkomst, categorie1 as reden, inhoudsindicatie, uitspraak, feitEnRechtPerAlinea,
+  meta.rechtsgebied, oordeel, aantalwoorden, positive, negative, anger, anticipation, disgust, fear, joy, sadness, surprise, trust
+FROM onderzoek
+LEFT JOIN uitspraaktekst ON uitspraaktekst.ecli = onderzoek.ecli
+LEFT JOIN meta ON meta.ecli = onderzoek.ecli
+WHERE onderzoek.ecli NOT LIKE 'ECLI:NL:RBGEL:2018:4813'
+AND tekst1 = 'wrakingszaak';
+  ")
+Multivariabelen <- dbGetQuery(conThuis, WrakingVeelVariabelenQuery)
+Multivariabelen <- Multivariabelen %>% 
+  select(-wrakingszaak) %>% 
+  mutate(uitkomst = as.factor(uitkomst)) %>% 
+  mutate(reden = as.factor(reden)) %>% 
+  mutate(uitspraak = gsub(pattern = "<(.*?)>", " ", uitspraak)) %>% 
+  mutate(uitspraak = stripWhitespace(uitspraak)) %>% 
+  mutate(uitspraak = trimws(uitspraak)) %>% 
+  mutate(inhoudsindicatie = gsub(pattern = "<(.*?)>", " ", inhoudsindicatie)) %>% 
+  mutate(inhoudsindicatie = stripWhitespace(inhoudsindicatie)) %>% 
+  mutate(inhoudsindicatie = trimws(inhoudsindicatie)) %>% 
+  separate(feitEnRechtPerAlinea, c("a", "b", "c"), sep=" \\| ") %>% 
+  separate(a, c("a", "feiten"), sep=" ") %>% 
+  separate(b, c("b", "recht"), sep=" ") %>%
+  separate(c, c("c", "c2", "totaal"), sep=" ") %>%
+  mutate(feiten=as.numeric(feiten)) %>% 
+  mutate(recht=as.numeric(recht)) %>%
+  mutate(totaal=as.numeric(totaal)) %>%
+  select(-a, -b, -c, -c2) %>% 
+  mutate(positive = positive/aantalwoorden) %>% 
+  mutate(negative = negative/aantalwoorden) %>% 
+  mutate(anger = anger/aantalwoorden) %>% 
+  mutate(anticipation = anticipation/aantalwoorden) %>% 
+  mutate(disgust = disgust/aantalwoorden) %>% 
+  mutate(fear = fear/aantalwoorden) %>% 
+  mutate(joy = joy/aantalwoorden) %>% 
+  mutate(sadness = sadness/aantalwoorden) %>% 
+  mutate(surprise = surprise/aantalwoorden) %>% 
+  mutate(trust = trust/aantalwoorden) %>% 
+  mutate(lengteUitspraak=nchar(uitspraak)) %>%
+  mutate(rechtsgebied = ifelse(grepl("Bestuurs", rechtsgebied)==TRUE, "bestuursrecht", rechtsgebied)) %>% 
+  mutate(rechtsgebied = ifelse(grepl("Civiel", rechtsgebied)==TRUE, "civielRecht", rechtsgebied)) %>% 
+  mutate(rechtsgebied = ifelse(grepl("Straf", rechtsgebied)==TRUE, "strafrecht", rechtsgebied)) %>%
+  mutate(rechtsgebied = ifelse(is.na(rechtsgebied), "onbekend", rechtsgebied)) %>% 
+  mutate(rechtsgebied = as.factor(rechtsgebied)) %>% 
+  mutate(instantie = ifelse(grepl(":RB", ecli)==TRUE, "rechtbank", "")) %>%   
+  mutate(instantie = ifelse(grepl(":GH", ecli)==TRUE, "hof", instantie)) %>%   
+  mutate(instantie = ifelse(grepl(":HR", ecli)==TRUE, "hogeRaad", instantie)) %>%   
+  mutate(instantie = ifelse(grepl(":RVS", ecli)==TRUE, "raadVanState", instantie)) %>%   
+  mutate(instantie = ifelse(instantie=="", "overig", instantie)) %>%   
+  mutate(instantie = as.factor(instantie)) %>% 
+  # mutate(instantie = ifelse(grepl(":Rb", ecli)==TRUE, "rechtbank", "rechtsgebied")) %>%   
+  as_tibble()
+
+saveRDS(Multivariabelen, file = "Wraking\\Multivariabelen.rds")
+# Dataset <- readRDS(file = "D:\\Uitwisselmap\\Projecten\\Ecli\\Rstudio\\Wraking\\Multivariabelen.rds") 
+
+
 # Eventueel: Zoeken op tekenreeks wraking:
 WrakingQuery <- paste(
   "SELECT uitspraaktekst.ecli, inhoudsindicatie, uitspraak
@@ -59,5 +119,3 @@ AND uitspraaktekst.ecli NOT LIKE '%NL:PHR%';"
 Dataset <- dbGetQuery(conThuis, WrakingQuery)
 saveRDS(Dataset, file = "Wraking_Trefwoord.rds")
 Dataset <- readRDS(file = "D:\\Uitwisselmap\\Projecten\\Ecli\\Rstudio\\Wraking_Trefwoord.rds") 
-
-  
